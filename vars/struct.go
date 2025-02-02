11c5371a74203d54a4ba277cabfd43f72f7d3f9c
Kishalay Bhattacharya <bhattacharyakishalay@gmail.com>
2025-02-02 11:41:41 +0530

    global variables

---
+++ vars/struct.go
@@ -0,0 +1,11 @@
+package vars
+
+type BookedTicketData struct {
+	FirstName   string
+	LastName    string
+	Mail        string
+	Tickets     int
+	SelectedRow string
+	Bill        int
+	BookingCode string
+}
--- vars/variables.go
+++ vars/variables.go
@@ -1,7 +1,13 @@
 package vars
 
 var (
-	RemainingTickets   = 50
-	BookingList        []string
-	BookingListDetails []map[string]string
+	RemainingTickets = 50
+
+	// lists
+	BookingList []string
+	// BookingListDetails []BookedTicketData
+
+	// maps
+	TicketCodesList   = make(map[string]bool)
+	BookingDetailsMap = make(map[string]BookedTicketData)
 )
