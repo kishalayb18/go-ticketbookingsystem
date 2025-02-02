63309a2fbea6005d2f47ce87227ca930533c2c28
Kishalay Bhattacharya <bhattacharyakishalay@gmail.com>
2025-02-02 12:27:13 +0530

    field modified with bills

--- process/getBookingDetail.go
+++ process/getBookingDetail.go
@@ -2,13 +2,14 @@ package process
 
 import "github.com/kishalayb18/go-ticketbookingsystem/vars"
 
-func GetBookingDetails(bookingCode string) (string, string, string, int) {
+func GetBookingDetails(bookingCode string) (string, string, string, int, int) {
 	firstname := vars.BookingDetailsMap[bookingCode].FirstName
 	lastName := vars.BookingDetailsMap[bookingCode].LastName
 	fullName := firstname + " " + lastName
 	mail := vars.BookingDetailsMap[bookingCode].Mail
 	selectedRow := vars.BookingDetailsMap[bookingCode].SelectedRow
 	numberofTickets := vars.BookingDetailsMap[bookingCode].Tickets
+	totalBill := vars.BookingDetailsMap[bookingCode].Tickets
 
-	return fullName, mail, selectedRow, numberofTickets
+	return fullName, mail, selectedRow, numberofTickets, totalBill
 }
--- process/postBookingDetails.go
+++ process/postBookingDetails.go
@@ -8,11 +8,12 @@ import (
 
 func PostBookingDetails(bookingCode string) {
 	if vars.TicketCodesList[bookingCode] { // this is mapped with bool
-		fullName, mail, selectedRow, numberofTickets := GetBookingDetails(bookingCode)
+		fullName, mail, selectedRow, numberofTickets, totalBill := GetBookingDetails(bookingCode)
 		fmt.Printf("Name: %v \n", fullName)
 		fmt.Printf("Mail: %v \n", mail)
 		fmt.Printf("Selected row: %v\n", selectedRow)
 		fmt.Printf("Total tickets: %v \n", numberofTickets)
+		fmt.Printf("Total Bill: %v \n", totalBill)
 		fmt.Println("")
 	} else {
 		fmt.Println("Booking Code is not correct")
